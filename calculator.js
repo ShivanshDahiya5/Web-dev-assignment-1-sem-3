b25e8146660ec23a0ef8fb11003e68987ce93fb8
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-07 21:22:32 +0530

    variable declarations for operation and numbers in calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,6 @@
-// let operation = process.argv[2]
-// let n1 = Number(process.argv[3])
-// let n2 = Number(process.argv[4])
+let operation = process.argv[2]
+let n1 = Number(process.argv[3])
+let n2 = Number(process.argv[4])
 
 // if(operation === 'add'){
 //     console.log(n1 + n2);
