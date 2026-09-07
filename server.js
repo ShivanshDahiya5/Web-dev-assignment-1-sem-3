791e17d7b81d198174416d924ca7525ebad33d79
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-07 21:25:52 +0530

    enable route handling for about and contact pages

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const server = http.createServer((req, res)=>{
     if(req.url == '/' && req.method === 'GET'){
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('Welcome to Backend');
-//     }else if(req.url == '/about' && req.method ==='GET'){
-//         res.writeHead(200, {'Content': 'text/plain'});
-//         res.end('Welcome to About Page');
-//     }else if(req.url == '/contact' && req.method === 'GET'){
-//         res.writeHead(200, {'Content-Type': 'text/plain'});
-//         res.end('Welcome to Contact Page')
-//     }else{
-//         res.end('404 not found');
-//     }
-// })
+    }else if(req.url == '/about' && req.method ==='GET'){
+        res.writeHead(200, {'Content': 'text/plain'});
+        res.end('Welcome to About Page');
+    }else if(req.url == '/contact' && req.method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('Welcome to Contact Page')
+    }else{
+        res.end('404 not found');
+    }
+})
 
 // const port = 3000
 // server.listen(port, ()=>{
