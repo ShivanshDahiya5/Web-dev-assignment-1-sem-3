90449482d3aac2f8574888f93bb4042874ae7dfd
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-07 21:24:01 +0530

    isEven function implementation in isEven.js

--- module/isEven.js
+++ module/isEven.js
@@ -1,9 +1,9 @@
-// function isEven(num){
-//     if(num % 2 === 0){
-//         return 'Number is Even';
-//     }else {
-//         return 'Number is Odd';
-//     }
-// }
+function isEven(num){
+    if(num % 2 === 0){
+        return 'Number is Even';
+    }else {
+        return 'Number is Odd';
+    }
+}
 
-// module.exports = {isEven}
+module.exports = {isEven}
