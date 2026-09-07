40755e032cc727484afd804824c1de6d8ebd9c50
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-07 21:23:05 +0530

    addition and subtraction operations in calculator.js

--- calculator.js
+++ calculator.js
@@ -2,10 +2,10 @@ let operation = process.argv[2]
 let n1 = Number(process.argv[3])
 let n2 = Number(process.argv[4])
 
-// if(operation === 'add'){
-//     console.log(n1 + n2);
-// }else if(operation === 'sub'){
-//     console.log(n1 - n2);
+if(operation === 'add'){
+    console.log(n1 + n2);
+}else if(operation === 'sub'){
+    console.log(n1 - n2);
 // }else if(operation === 'multi'){
 //     console.log(n1 * n2);
 // }else if(operation === 'div'){
