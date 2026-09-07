9f79b34814766b8c915c2b96611f082a4631fe47
ShivanshDahiya5 <shivanshdahiya563@gmail.com>
2026-09-07 21:28:35 +0530

    dice rolling functionality in Dice.js

--- Dice.js
+++ Dice.js
@@ -1,5 +1,5 @@
-// const crypto = require('crypto');
+const crypto = require('crypto');
 
-// let number = crypto.randomInt(1,7);
+let number = crypto.randomInt(1,7);
 
-// console.log("Dice Rolled: ", number); 
+console.log("Dice Rolled: ", number); 
